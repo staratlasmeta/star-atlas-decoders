4009ab8247691251772b95ea5458eef9e87f9040
Tanner Donovan <tanner.donovan@staratlas.com>
2025-10-08 11:12:39 -0700

    feat: sage-holosim patch 04 instructions mining (#21)

--- carbon-decoders/sage-holosim-decoder/src/instructions/start_mining_asteroid.rs
+++ carbon-decoders/sage-holosim-decoder/src/instructions/start_mining_asteroid.rs
@@ -12,9 +12,17 @@ pub struct StartMiningAsteroid {
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash, serde::Serialize, serde::Deserialize)]
 pub struct StartMiningAsteroidInstructionAccounts {
-    pub game_accounts_fleet_and_owner: solana_pubkey::Pubkey,
+    // GameAndGameStateAndFleetAndOwnerMut expansion
+    pub key: solana_pubkey::Pubkey,
+    pub owning_profile: solana_pubkey::Pubkey,
+    pub owning_profile_faction: solana_pubkey::Pubkey,
+    pub fleet: solana_pubkey::Pubkey,
+    pub game_id: solana_pubkey::Pubkey,
+    pub game_state: solana_pubkey::Pubkey,
     pub fleet_fuel_token_account: solana_pubkey::Pubkey,
-    pub starbase_and_starbase_player: solana_pubkey::Pubkey,
+    // StarbaseMutAndStarbasePlayer expansion
+    pub starbase: solana_pubkey::Pubkey,
+    pub starbase_player: solana_pubkey::Pubkey,
     pub mine_item: solana_pubkey::Pubkey,
     pub resource: solana_pubkey::Pubkey,
     pub planet: solana_pubkey::Pubkey,
@@ -27,17 +35,35 @@ impl carbon_core::deserialize::ArrangeAccounts for StartMiningAsteroid {
         accounts: &[solana_instruction::AccountMeta],
     ) -> Option<Self::ArrangedAccounts> {
         let mut iter = accounts.iter();
-        let game_accounts_fleet_and_owner = next_account(&mut iter)?;
+
+        // GameAndGameStateAndFleetAndOwnerMut expansion
+        let key = next_account(&mut iter)?;
+        let owning_profile = next_account(&mut iter)?;
+        let owning_profile_faction = next_account(&mut iter)?;
+        let fleet = next_account(&mut iter)?;
+        let game_id = next_account(&mut iter)?;
+        let game_state = next_account(&mut iter)?;
+
         let fleet_fuel_token_account = next_account(&mut iter)?;
-        let starbase_and_starbase_player = next_account(&mut iter)?;
+
+        // StarbaseMutAndStarbasePlayer expansion
+        let starbase = next_account(&mut iter)?;
+        let starbase_player = next_account(&mut iter)?;
+
         let mine_item = next_account(&mut iter)?;
         let resource = next_account(&mut iter)?;
         let planet = next_account(&mut iter)?;
 
         Some(StartMiningAsteroidInstructionAccounts {
-            game_accounts_fleet_and_owner,
+            key,
+            owning_profile,
+            owning_profile_faction,
+            fleet,
+            game_id,
+            game_state,
             fleet_fuel_token_account,
-            starbase_and_starbase_player,
+            starbase,
+            starbase_player,
             mine_item,
             resource,
             planet,
